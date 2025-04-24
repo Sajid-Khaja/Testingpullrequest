c663e045cd0607f8b187a80292abdbf281dd69cc
sajiid sultan <sajiidsultan@sajiids-MacBook-Air.local>
2025-04-24 15:56:55 +0530

    add one feature

--- Crud_App/models.py
+++ Crud_App/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Operation(models.Model):
-    Name=models.CharField( max_length=50)
-    Last_Name=models.CharField( max_length=50)
-    Address=models.CharField(max_length=50)
-    Postal_code=models.IntegerField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+# class Operation(models.Model):
+#     Name=models.CharField( max_length=50)
+#     Last_Name=models.CharField( max_length=50)
+#     Address=models.CharField(max_length=50)
+#     Postal_code=models.IntegerField()
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.Name
+#     def __str__(self):
+#         return self.Name
 
 
  
